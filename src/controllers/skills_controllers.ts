741591c7285272e0608346209b28e3557cb243b1
= <=>
2023-07-03 17:44:38 -0300

    just trying to survive

--- src/controllers/skills_controllers.ts
+++ src/controllers/skills_controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import Services_person from "../services/skills_services"
-import { BAD_REQUEST, OK } from "http-status"
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from "http-status"
 import { SkillFormat } from "schemas/skills_schemas"
 
 
@@ -8,7 +8,7 @@ async function create(req: Request, res: Response){
     const skill : SkillFormat = req.body; 
     try {
         await Services_person.createNewSkill(skill)
-        return res.status(OK).send()
+        return res.sendStatus(OK)
     } catch (error) {
         return res.sendStatus(BAD_REQUEST)
     }
@@ -16,46 +16,49 @@ async function create(req: Request, res: Response){
 
 async function getById(req: Request, res: Response){
     const { id } : {id?:string} = req.params
-    
+    const parsed = Number.parseInt(id)
+    console.log(parsed)
     try {
-        if(isNaN(Number(id)) || !id){
+        if(isNaN(parsed)){
             return res.sendStatus(BAD_REQUEST)
         }
         
 
-        return res.status(OK).send(id)
+        return res.status(OK).send({parsed})
     } catch (error) {
-    
+        return res.sendStatus(INTERNAL_SERVER_ERROR)
     }
 }
 
 async function updateOneById(req: Request, res: Response){
     const { id } : {id?:string} = req.params
-    
+    const parsed = Number.parseInt(id)
+    console.log(parsed)
     try {
-        if(isNaN(Number(id)) || !id){
+        if(isNaN(parsed)){
             return res.sendStatus(BAD_REQUEST)
         }
         
 
-        return res.status(OK).send(id)
+        return res.status(OK).send({parsed})
     } catch (error) {
-    
+        return res.sendStatus(INTERNAL_SERVER_ERROR)
     }
 }
 
 async function deleteOneById(req: Request, res: Response){
     const { id } : {id?:string} = req.params
-    
+    const parsed = Number.parseInt(id)
+    console.log(parsed)
     try {
-        if(isNaN(Number(id)) || !id){
+        if(isNaN(parsed)){
             return res.sendStatus(BAD_REQUEST)
         }
         
 
-        return res.status(OK).send(id)
+        return res.status(OK).send({parsed})
     } catch (error) {
-    
+        return res.sendStatus(INTERNAL_SERVER_ERROR)
     }
 }
 const controllerSkills = {
--- src/middlewares/generics_middlewares.ts
+++ src/middlewares/generics_middlewares.ts
@@ -1,24 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-import { unauthorizedError } from '../errors/index';
+import { conflictError, unauthorizedError } from '../errors/index';
 
-export async function authenticateToken(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-  const authHeader = req.header('Authorization');
-  if (!authHeader) return generateUnauthorizedResponse(res);
+// export async function authenticateToken(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+//   const authHeader = req.header('Authorization');
+//   if (!authHeader) return generateUnauthorizedResponse(res);
 
-  const token = authHeader.split(' ')[1];
-  if (!token) return generateUnauthorizedResponse(res);
+//   const token = authHeader.split(' ')[1];
+//   if (!token) return generateUnauthorizedResponse(res);
 
-  try {
+//   try {
 
-    //aqui
+//     //aqui
 
-    return next();
-  } catch (err) {
-    return generateUnauthorizedResponse(res);
-  }
+//     return next();
+//   } catch (err) {
+//     return generateUnauthorizedResponse(res);
+//   }
+// }
+
+
+export function parseId(id:string){
+    const parsed = Number.parseInt(id)
+    if(isNaN(parsed)){
+        throw conflictError
+    }
+    return parsed
 }
+
 
 function generateUnauthorizedResponse(res: Response) {
   res.status(httpStatus.UNAUTHORIZED).send(unauthorizedError());
